1bbfa53e772eaf532c3ab2a2a69a03275a062310
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2026-02-27 19:54:16 -0300

    adiciona app layout

---
+++ lista-tarefas-laravel/src/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Lista de Tarefas')</title>
+</head>
+
+<body>
+    <div>
+        <header>
+            <h1>Lista de Tarefas</h1>
+        </header>
+    </div>
+
+    @if(session('ok'))
+    <div>
+        {{ session('ok') }}
+    </div>
+    @endif
+
+    @yield('content')
+</body>
+
+</html>
